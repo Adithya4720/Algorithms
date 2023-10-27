360a46fac10233b6484f08e0a0c136efa00293ba
Adithya4720 <hegdeadithyak@gmail.com>
2023-10-27 16:06:46 +0530

    Terenery Search

---
+++ C++/Searching/ternerysearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool ternerysearch(int *arr,int start,int low,int key){
+    int mid1= start + (low-start)/3;
+    int mid2 = low - (low-start)/3;
+    if(arr[mid1] == key){
+        return 1;
+    }
+    else if(key>arr[mid1] && key<arr[mid2]){
+        return ternerysearch(arr,mid1+1,mid2-1,key);
+    }
+    else if(key<arr[mid1]){
+        return ternerysearch(arr,start,mid1-1,key);
+    }
+    else if(key>arr[mid2]){
+        return ternerysearch(arr,mid2+1,low,key);
+    }
+};
+
+int main(){
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i=0;i<size;i++){
+        int num;
+        cin >> num;
+        arr[i] = num;
+    }
+    int key;
+    cin >> key;
+    cout << ternerysearch(arr,0,size-1,key);
+
+}

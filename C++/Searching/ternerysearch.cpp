694ae67c71232643f97bbdda6e29aed1f72967dc
Adithya4720 <hegdeadithyak@gmail.com>
2023-10-28 10:07:58 +0530

    Re-Check

--- C++/Searching/ternerysearch.cpp
+++ C++/Searching/ternerysearch.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+
+//THIS RUNS ON O(LOGN) Rruntime
 bool ternerysearch(int *arr,int start,int low,int key){
     int mid1= start + (low-start)/3;
     int mid2 = low - (low-start)/3;

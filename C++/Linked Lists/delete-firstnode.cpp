7f15220cfd0ef2ec00bf50146dcf8af072b14ec5
Adithya4720 <hegdeadithyak@gmail.com>
2023-10-30 11:28:09 +0530

    Deleting First Nodes

---
+++ C++/Linked Lists/delete-firstnode.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data= x;
+        next = NULL;
+    }
+};
+
+Node *firstNode(Node *head){
+    if (head == NULL)
+        return NULL;
+    else
+    {
+        Node *temp = head->next;
+        delete (head);
+        return temp;
+    }
+}
+void printNode( Node *head){
+    Node *curr = head;
+    while(curr!=NULL){
+        cout << curr->data << endl;
+        curr = curr->next;
+    }
+}
+int main(){
+    Node *head= new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+    Node *newhead;
+    newhead = firstNode(head);
+    printNode(newhead);
+
+    return 0;
+}
